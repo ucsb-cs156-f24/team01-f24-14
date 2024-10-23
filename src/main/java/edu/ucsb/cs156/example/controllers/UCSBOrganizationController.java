1562945bc39cdc7955799b0bc41a894d95519696
Kevin Yan <kevinyan@169-231-17-136.wireless.ucsb.edu>
2024-10-23 16:38:39 -0700

    Testing to fix git errrors

--- src/main/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationController.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import edu.ucsb.cs156.example.entities.UCSBOrganization;
 import edu.ucsb.cs156.example.repositories.UCSBOrganizationRepository;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
