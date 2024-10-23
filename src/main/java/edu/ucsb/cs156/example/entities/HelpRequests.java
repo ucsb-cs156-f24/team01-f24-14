a9ced7cf628e1f56b9c496513645e1462ec6c3d3
Jennifer Zhu <JenniferZhu@ucsb.edu>
2024-10-23 12:26:13 -0700

    saving changes for task 2

---
+++ src/main/java/edu/ucsb/cs156/example/entities/HelpRequests.java
@@ -0,0 +1,32 @@
+package edu.ucsb.cs156.example.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * This is a JPA entity that represents a restaurant.
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "HelpRequests")
+public class HelpRequests {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private String requesterEmail;
+  private String teamId;
+  private String tableOrBreakoutRoom;
+  private LocalDateTime requestTime;
+  private String explanation;
+  private boolean solved;
+}
