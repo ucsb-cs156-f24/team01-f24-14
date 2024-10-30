acf3c08386559f65c6a9d08ff9536196659b28d0
Jennifer Zhu <JenniferZhu@ucsb.edu>
2024-10-29 17:33:01 -0700

    finished

--- src/main/java/edu/ucsb/cs156/example/controllers/HelpRequestsController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/HelpRequestsController.java
@@ -5,17 +5,22 @@ import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.ucsb.cs156.example.entities.HelpRequests;
+import edu.ucsb.cs156.example.errors.EntityNotFoundException;
 import edu.ucsb.cs156.example.repositories.HelpRequestsRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -75,5 +80,50 @@ public class HelpRequestsController extends ApiController {
         helpRequest.setRequestTime(requestTime);
 
         return helpRequestsRepository.save(helpRequest);
+    }
+    // GET (by ID / single entry): Gets a single help request by id
+    @Operation(summary= "Get a single help request")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("")
+    public HelpRequests getById(
+        @Parameter(name="id") @RequestParam Long id) {
+        HelpRequests helpRequest = helpRequestsRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(HelpRequests.class, id));
+
+        return helpRequest;
+    }
+    @Operation(summary= "Update a single help request")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping("")
+    public HelpRequests updateHelpRequest(
+            @Parameter(name="id") @RequestParam Long id,
+            @RequestBody @Valid HelpRequests incoming) {
+
+        HelpRequests helpRequest = helpRequestsRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(HelpRequests.class, id));
+
+        helpRequest.setRequesterEmail(incoming.getRequesterEmail());
+        helpRequest.setTeamId(incoming.getTeamId());
+        helpRequest.setTableOrBreakoutRoom(incoming.getTableOrBreakoutRoom());
+        helpRequest.setExplanation(incoming.getExplanation());
+        helpRequest.setSolved(incoming.getSolved());
+        helpRequest.setRequestTime(incoming.getRequestTime());
+
+        helpRequestsRepository.save(helpRequest);
+
+        return helpRequest;
+    }
+
+    // DELETE : deletes a single entry in the data table via ID.
+    @Operation(summary= "Delete a help request")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("")
+    public Object deleteHelpRequest(
+            @Parameter(name="id") @RequestParam Long id) {
+        HelpRequests helpRequest = helpRequestsRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(HelpRequests.class, id));
+
+        helpRequestsRepository.delete(helpRequest);
+        return genericMessage("HelpRequest with id %s deleted".formatted(id));
     }
 }
